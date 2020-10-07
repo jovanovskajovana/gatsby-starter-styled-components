d7dce0a97245f93fb79d50c54359eb639e168325
jovanovskajovana <jovanovska.jovana1@gmail.com>
2020-10-07 11:07:49 +0200

    Remove ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,7 @@
-import React from 'react'
+// import React from 'react'
 
-import Page from './src/components/Page'
+// import Page from './src/components/Page'
 
-export function wrapPageElement({ element, props }) {
-  return <Page {...props}>{element}</Page>
-}
+// export function wrapPageElement({ element, props }) {
+//   return <Page {...props}>{element}</Page>
+// }
